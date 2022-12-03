80b2d5661556190e61b8cfbc956a904a1b4779a4
mhg-16 <hammahadi2006@gmail.com>
2022-12-03 11:13:42 +0100

    setup up Navbar component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
+
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">Amekagaru No Sato Store</Link>
+      </p>
+
+      <button type='button' className='cart-icon' onClick={() => console.log("Clicked")}>
+        <AiOutlineShopping />
+        <span className='cart-item-qty'> 1</span>
+      </button>
+    </div>
   )
 }
 
